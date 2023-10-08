147e6d57353cf264ffc464e2bf5fa268ea38da38
ymk74 <swimmer.shark12@gmail.com>
2023-10-09 01:45:27 +0300

    add coffee page & create accounts app

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+
 # Create your views here.
 
 
 
+
 def index(request):
-    return render(request , 'pages\index.html')
+
+    return render(request , 'pages/index.html')
+
 
 
 def about(request):
+
     return render(request , 'pages/about.html')
+
+
+
+
+def coffee(request):
+    return render(request , 'pages/coffee.html')
--- project/urls.py
+++ project/urls.py
@@ -26,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('' , include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
