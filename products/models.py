3a480070241f72b7278f7562a7d51631dabb4dd6
ymk74 <swimmer.shark12@gmail.com>
2023-10-09 18:09:11 +0300

    add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products')
+    active = models.BooleanField(default=True)
+    created_at = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
