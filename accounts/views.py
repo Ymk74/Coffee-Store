61f6a987722ec552541a904c79d71961daf2a8f8
ymk74 <swimmer.shark12@gmail.com>
2023-10-16 22:42:36 +0300

    add name in signin & sign up page to appear in browser search bar

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 
 
 def signin(request):
+    if request.GET:
+        messages.info(request, 'Test Message1')
+        
     return render(request , 'signin.html')
     
-
 
 
 def signup(request):
