1469634ca3ad587a28ac12e732e54b9f8ccdc29e
ymk74 <swimmer.shark12@gmail.com>
2023-10-17 00:05:20 +0300

    add csrf token to signin & sign up & profile page

--- accounts/views.py
+++ accounts/views.py
@@ -4,23 +4,11 @@ from django.contrib import messages
 
 
 def signin(request):
-    if request.GET:
-        messages.info(request, 'Test Message1')
-        messages.success(request, 'Test Message1')
-        messages.error(request, 'Test Message1')
-
     return render(request , 'signin.html')
     
 
 
 def signup(request):
-    if request.POST:
-        messages.info(request, 'This is post')
-
-    if request.GET:
-        messages.info(request, 'This is get')
-
-
     return render(request , 'signup.html')
     
 
