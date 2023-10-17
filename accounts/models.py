a346b37083fd5da94dfdb8026bc8af21c757ad1a
ymk74 <swimmer.shark12@gmail.com>
2023-10-17 18:16:09 +0300

    create account models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=60)
+    address2 = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    state = models.CharField(max_length=60)
+    zip_number = models.CharField(max_length=6)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib import messages
 # Create your views here.
 
 
 def signin(request):
-    return render(request , 'signin.html')
+    if request.method == 'POST' and 'btn_login' in request.POST:
+
+        messages.info(request,'test and btn_login')
+
+        return redirect('signin')
+    else:
+        return render(request , 'signin.html')
     
 
 
 def signup(request):
-    return render(request , 'signup.html')
+    if request.method == 'POST' and 'btn_signup' in request.POST:
+        messages.info(request,'test and btn_signup')
+        return redirect('signup')
+    else:
+        return render(request , 'signup.html')
     
 
 
 def profile(request):
-    return render(request , 'profile.html')
+    if request.method == 'POST' and 'btn_save' in request.POST:
+        messages.info(request,'test and btn_save')
+        return redirect('profile')
+    else:
+        return render(request , 'profile.html')
     
