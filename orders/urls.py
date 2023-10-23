a9ae00687c52e44280a4666d6c90f18d680bc791
ymk74 <swimmer.shark12@gmail.com>
2023-10-23 13:21:37 +0300

    create orders urls

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('add_to_cart' ,views.add_to_cart , name = 'add_to_cart'),
+        path('cart',views.cart,name='cart'),
+    path('remove_cart<int:id>',views.removecart,name='removecart'),
+    path('remove_cart<int:id>',views.removecart,name='removecart'),
+    path('add_qty/<int:id>',views.add_qty,name='add_qty'),
+    path('sub_qty/<int:id>',views.sub_qty,name='sub_qty'),
+    path('payment',views.payment,name='payment'),
 ]
